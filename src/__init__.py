2fae4a6f30212f81cb15364804e995f9b21e046e
lucacorbucci <tankado@live.it>
2019-04-21 17:54:51 +0200

    Some changes

--- src/__init__.py
+++ src/__init__.py
@@ -19,24 +19,27 @@ else:
     if(len(sys.argv) > 3):
         maxBucketList = int(sys.argv[3])
     
-    # Creazione del coordinatore 
-    coordinator = coordinator(numNodes, idLen, maxBucketList)
-    
+    if(numNodes > pow(2,idLen)):
+        print "Lunghezza degli identificatori non sufficiente"
+    else:
+        # Creazione del coordinatore 
+        coordinator = coordinator(numNodes, idLen, maxBucketList)
+        
 
-    
-    # Per n-1 volte deve essere eseguita la fase di costruzione della
-    # routing table.
-    for i in range(numNodes-1):
-        # Generazione del nodo che vuole entrare nella rete
-        joiningNode = coordinator.generateNode(idLen)      
-        # Calcolo del nodo bootstrap
-        bootstrapNode = coordinator.generateBootstrapNode()
-        #print ""
-        #print "joininig: ", joiningNode.id
-        #print "bootstrap", bootstrapNode.id
-        # Inserimento del nuovo nodo nella rete
-        coordinator.join(joiningNode, bootstrapNode)
-        #print "bootstrap Node: ", bootstrapNode.id
-        #print "joining Node: ", joiningNode.id
-    coordinator.debug()
+        
+        # Per n-1 volte deve essere eseguita la fase di costruzione della
+        # routing table.
+        for i in range(numNodes-1):
+            # Generazione del nodo che vuole entrare nella rete
+            joiningNode = coordinator.generateNode(idLen)      
+            # Calcolo del nodo bootstrap
+            bootstrapNode = coordinator.generateBootstrapNode()
+            #print ""
+            #print "joininig: ", joiningNode.id
+            #print "bootstrap", bootstrapNode.id
+            # Inserimento del nuovo nodo nella rete
+            coordinator.join(joiningNode, bootstrapNode)
+            #print "bootstrap Node: ", bootstrapNode.id
+            #print "joining Node: ", joiningNode.id
+        coordinator.debug()
     
